6032379e9746bf53e73326db0a58157e4eb52fce
Ian Moritz <icm35@cornell.edu>
2020-07-14 17:19:55 -0400

    data and initial nb

---
+++ convnet.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+tf.__version__
+
+
+# In[2]:
+
+
+#PARAMETERS
+
+
+# In[ ]:
+
+
+
+
